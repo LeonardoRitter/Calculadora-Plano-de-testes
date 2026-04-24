85f20cea46320d19cce444483c77ab958c407f78
LeonardoRitter <leonardoritter_@hotmail.com>
2026-04-23 21:36:26 -0300

    Plano

---
+++ script.js
@@ -0,0 +1,37 @@
+let numeroAtual = "";
+let numeroAnterior = "";
+let operacao = "";
+
+function clicarNumero(num) {
+    numeroAtual += num;
+    document.getElementById("visor").value = numeroAtual;
+}
+
+function definirOperacao(op) {
+    numeroAnterior = numeroAtual;
+    numeroAtual = "";
+    operacao = op;
+} 
+
+function calcular() {
+    let resultado;
+
+    let n1 = parseFloat(numeroAnterior);
+    let n2 = parseFloat(numeroAtual);
+
+    if (operacao === "+") resultado = n1 + n2;
+    if (operacao === "-") resultado = n1 - n2;
+    if (operacao === "*") resultado = n1 * n2;
+    if (operacao === "/") resultado = n1 / n2;
+
+    document.getElementById("visor").value = resultado;
+    numeroAtual = resultado;
+}
+
+function limpar() {
+    numeroAtual = "";
+    numeroAnterior = "";
+    operacao = "";  
+    document.getElementById("visor").value = "";
+}
+     
